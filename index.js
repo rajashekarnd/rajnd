a4c764919887d7bb01b13bcd73a85cfd2bb71f7b
rajashekarnd <rajashekarnd13598@gmail.com>
2020-11-15 21:46:58 +0530

    ramndcommit

---
+++ index.js
@@ -0,0 +1,17 @@
+function studentmarks(marks,percentage){
+    percentage<35? console.log("fail"): console.log("pass");
+}
+studentmarks("ramesh",70);
+function studentmarks(marks,percentage){
+    percentage<35? console.log("fail"): console.log("pass")
+}
+studentmarks("ramesha",25);
+
+
+
+
+
+function celsiustofahrenheit(temp){
+    fahrenheit=temp*9/5+32; console.log("temp is" +fahrenheit);
+}
+celsiustofahrenheit(0);
